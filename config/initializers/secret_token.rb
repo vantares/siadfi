d2bb0b94da420abb0f92492c99ad5623166ebfaf
Alvarez, Juan Carlos <alvarezvelez.juancarlos@gmail.com>
2013-04-17 17:29:11 -0600

    Start Ruby on Rails project repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siadfi::Application.config.secret_token = '39d83a8c75b161dcc7e8b4c9627c443e4200445beadc598e0b93b3e015f2d4f45f5a6be25b31a2dbb8c630902cdd6e59e08d9b1ad4fb93484c07bc6e9130cfab'
